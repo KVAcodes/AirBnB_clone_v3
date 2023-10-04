245ac2c3335f882961f07c49e7183a5f8b69c4a5
KVAcodes <kadiriavictor@gmail.com>
2023-10-04 17:10:03 +0000

    added API for users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,79 +1,81 @@
 #!/usr/bin/python3
-""" View that handles all default RESTFul API actions for the State
+""" View that handles all default RESTFul API actions for the User
 object.
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.user import User
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-@app_views.route('/states/<state_id>',
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>',
                  methods=['GET'], strict_slashes=False)
-def get_state_or_states(state_id=None):
-    """Retrieves a list of all State objects or a State with
-    id matching state_id.
+def get_user_or_users(user_id=None):
+    """Retrieves a list of all User objects or an User object with
+    id matching user_id.
     """
-    if state_id:
-        state = storage.get(State, state_id)
-        if state:
-            return jsonify(state.to_dict())
+    if user_id:
+        user = storage.get(User, user_id)
+        if user:
+            return jsonify(user.to_dict())
         else:
             abort(404)
     else:
-        states = [state.to_dict() for state in storage.all(State).values()]
-        return jsonify(states)
+        users = [user.to_dict() for user in
+                 storage.all(User).values()]
+        return jsonify(users)
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/users/<user_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_state(state_id):
-    """Deletes a State object with id matching state_id.
+def delete_user(user_id):
+    """Deletes an User object with id matching user_id.
     """
-    state = storage.get(State, state_id)
-    if state:
-        storage.delete(state)
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
         storage.save()
         return jsonify({}), 200
     else:
         abort(404)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
-    """Creates a State object.
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates an User object.
     """
     if request.is_json:
-        state_json = request.get_json()
-        if 'name' not in state_json:
-            abort(400, "Missing name")
-        else:
-            state_created = State(**state_json)
-            state_created.save()
-            return jsonify(state_created.to_dict()), 201
+        user_json = request.get_json()
+        if 'email' not in user_json:
+            abort(400, "Missing email")
+        if 'password' not in user_json:
+            abort(400, "Missing password")
+        user_created = User(**user_json)
+        user_created.save()
+        return jsonify(user_created.to_dict()), 201
     else:
         abort(400, "Not a JSON")
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/users/<user_id>',
                  methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """Updates the attributes of a State Object.
+def update_user(user_id):
+    """Updates the attributes of an User Object.
     """
-    stored_state = storage.get(State, state_id)
-    if stored_state:
+    stored_user = storage.get(User, user_id)
+    if stored_user:
         if request.is_json:
-            updated_dict = stored_state.to_dict()
-            keys_to_ignore = ['id', 'created_at', 'updated_at']
-            state_retrieved = request.get_json()
-            for key, val in state_retrieved.items():
+            updated_dict = stored_user.to_dict()
+            keys_to_ignore = ['id', 'email', 'created_at', 'updated_at']
+            user_retrieved = request.get_json()
+            for key, val in user_retrieved.items():
                 if key not in keys_to_ignore and key in updated_dict:
                     updated_dict[key] = val
-            storage.delete(stored_state)
-            updated_state = State(**updated_dict)
-            updated_state.save()
-            return jsonify(updated_state.to_dict()), 200
+            storage.delete(stored_user)
+            updated_user = User(**updated_dict)
+            updated_user.save()
+            return jsonify(updated_user.to_dict()), 200
         else:
             abort(400, "Not a JSON")
     else:
